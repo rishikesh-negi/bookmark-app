16bfd20c56a55020ba11db5466975bad0b164a0a
Rishikesh Negi <rishinegi97@gmail.com>
2026-02-19 21:09:30 +0530

    Added route protection for all /account/** routes

--- app/account/layout.tsx
+++ app/account/layout.tsx
@@ -1,6 +1,12 @@
 import SideNavigation from "@/app/_components/SideNavigation";
+import { use } from "react";
+import { auth } from "../_lib/auth";
+import { redirect } from "next/navigation";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const session = use(auth());
+  if (!session) return redirect("/login");
+
   return (
     <div className="grid grid-cols-[16rem_1fr] h-full gap-12">
       <SideNavigation />
