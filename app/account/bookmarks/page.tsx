440d77a323b7d0c5a21b59c0bc4650d7c462caf9
Rishikesh Negi <rishinegi97@gmail.com>
2026-02-19 21:08:36 +0530

    Moved the user authorization logic to page layout

--- app/account/bookmarks/page.tsx
+++ app/account/bookmarks/page.tsx
@@ -1,14 +1,11 @@
 import BookmarksList from "@/app/_components/BookmarksList";
 import { auth, SessionWithUserId } from "@/app/_lib/auth";
 import { getBookmarks } from "@/app/_lib/data-service";
-import { redirect } from "next/navigation";
 import { use } from "react";
 
 export default function Page() {
   const session = use(auth()) as SessionWithUserId;
-  if (!session) return redirect("/login");
-
-  const bookmarks = use(getBookmarks(session.user.userId));
+  const bookmarks = use(getBookmarks(session!.user.userId));
 
   return <BookmarksList bookmarks={bookmarks} />;
 }
