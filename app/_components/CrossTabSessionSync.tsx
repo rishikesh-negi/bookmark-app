5f60e86a5e636fb487a3ee0ebe73752f104351b0
Rishikesh Negi <rishinegi97@gmail.com>
2026-02-21 18:29:05 +0530

    A client component to update session state across different browser tabs by listening to the broadcasted auth session status

---
+++ app/_components/CrossTabSessionSync.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { getBroadcastChannel } from "../_lib/broadcast";
+import { useSyncBookmarks } from "../hooks/useSyncBookmarks";
+
+export default function CrossTabSessionSync() {
+  const router = useRouter();
+  const { data: session } = useSession();
+  useSyncBookmarks();
+
+  useEffect(() => {
+    const channel = getBroadcastChannel();
+    if (!channel) return;
+
+    channel.onmessage = (e) => {
+      if (
+        e.data.sessionStatus === "authenticated" ||
+        e.data.sessionStatus === "unauthenticated"
+      ) {
+        console.log(e.data.sessionStatus);
+        router.refresh();
+      }
+    };
+  }, [router, session]);
+
+  return null;
+}
