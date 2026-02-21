0f30499b1ba989259e1fa8b0b72ab1685ed124a6
Rishikesh Negi <rishinegi97@gmail.com>
2026-02-21 18:33:15 +0530

    Removed the logic of redirection to the previous page that redirected to the login page

--- app/_components/SignInButton.tsx
+++ app/_components/SignInButton.tsx
@@ -1,12 +1,9 @@
 import Image from "next/image";
 import { signInAction } from "../_lib/actions";
 
-export default function SignInButton(
-  this: unknown,
-  { redirectTo }: { redirectTo?: string },
-) {
+export default function SignInButton() {
   return (
-    <form action={signInAction.bind(this, redirectTo)}>
+    <form action={signInAction}>
       <button className="flex items-center gap-4 text-lg text-brand-800 cursor-pointer border border-brand-800 transition-all px-7 py-3 hover:bg-brand-800 hover:text-brand-50 hover:[&>img]:scale-105">
         <Image
           src="https://authjs.dev/img/providers/google.svg"
