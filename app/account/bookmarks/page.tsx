163df979a7a98aa9e7e9263421a43e245844de12
Rishikesh Negi <rishinegi97@gmail.com>
2026-02-21 18:40:45 +0530

    Added page metadata (title) and redirection if no session is detected

--- app/account/bookmarks/page.tsx
+++ app/account/bookmarks/page.tsx
@@ -1,10 +1,17 @@
 import BookmarksList from "@/app/_components/BookmarksList";
 import { auth, SessionWithUserId } from "@/app/_lib/auth";
 import { getBookmarks } from "@/app/_lib/data-service";
+import { type Metadata } from "next";
+import { redirect } from "next/navigation";
 import { use } from "react";
+
+export const metadata: Metadata = {
+  title: "Bookmarks",
+};
 
 export default function Page() {
   const session = use(auth()) as SessionWithUserId;
+  if (!session) return redirect("/login");
   const bookmarks = use(getBookmarks(session!.user.userId));
 
   return <BookmarksList bookmarks={bookmarks} />;
