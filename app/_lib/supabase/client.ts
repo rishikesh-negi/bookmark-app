8cf2733d508a4ddcb57cb1489ddfd04483ab59b8
Rishikesh Negi <rishinegi97@gmail.com>
2026-02-25 18:54:24 +0530

    Reverted to default options for Supabase browser client

--- app/_lib/supabase/client.ts
+++ app/_lib/supabase/client.ts
@@ -5,11 +5,4 @@ import { createBrowserClient } from "@supabase/ssr";
 export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
-  {
-    auth: {
-      persistSession: true,
-      autoRefreshToken: true,
-      detectSessionInUrl: true,
-    },
-  },
 );
