a4abde9418bc8813ff1cb76a19af5f10f9dbf23d
Rishikesh Negi <rishinegi97@gmail.com>
2026-02-25 21:49:04 +0530

    Moved creation of Supabase Realtime WS outside the effect

--- app/hooks/useSyncBookmarksState.ts
+++ app/hooks/useSyncBookmarksState.ts
@@ -6,25 +6,23 @@ import { useUserDataOnClient } from "./useUserDataOnClient";
 export function useSyncBookmarksState() {
   const router = useRouter();
   const user = useUserDataOnClient();
+  const bookmarksChannel = supabase
+    .channel("bookmarks-channel")
+    .on(
+      "postgres_changes",
+      {
+        event: "*",
+        schema: "public",
+        table: "bookmarks",
+        filter: `owner=eq.${user?.id}`,
+      },
+      () => router.refresh(),
+    )
+    .subscribe();
 
   useEffect(() => {
-    const bookmarksChannel = supabase.channel("bookmarks-channel");
-
-    bookmarksChannel
-      .on(
-        "postgres_changes",
-        {
-          event: "*",
-          schema: "public",
-          table: "bookmarks",
-          filter: `owner=eq.${user?.id}`,
-        },
-        () => router.refresh(),
-      )
-      .subscribe();
-
     return () => {
       supabase.removeChannel(bookmarksChannel);
     };
-  }, [router, user]);
+  }, [bookmarksChannel]);
 }
