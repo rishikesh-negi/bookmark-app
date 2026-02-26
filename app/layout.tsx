111f415ebbd28e60da3dac0a0b9142a8ccd12271
Rishikesh Negi <rishinegi97@gmail.com>
2026-02-26 13:14:54 +0530

    Wrapped the client component responsible for syncing session and bookmarks state in a suspense boundary to ensure reliability in creation of Supabase Realtime WS

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Poppins } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import CrossTabAppStateSync from "./_components/CrossTabAppStateSync";
 import Header from "./_components/Header";
+import { Suspense } from "react";
 
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -30,7 +31,9 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${poppins.variable} antialiased bg-slate-100 text-slate-800 min-h-dvh flex flex-col font-sans`}>
-        <CrossTabAppStateSync />
+        <Suspense>
+          <CrossTabAppStateSync />
+        </Suspense>
         <div id="modal-root"></div>
         <Header />
 
