60e3f18717ad6866f6411df2c1f3f7f18bb5dfb5
Rishikesh Negi <rishinegi97@gmail.com>
2026-02-26 14:16:42 +0530

    Removed redundant imports

--- app/account/bookmarks/page.tsx
+++ app/account/bookmarks/page.tsx
@@ -1,11 +1,10 @@
 import BookmarksList from "@/app/_components/BookmarksList";
 import CrossTabBookmarksSync from "@/app/_components/CrossTabBookmarksSync";
-import Spinner from "@/app/_components/ui/Spinner";
 import { getBookmarks } from "@/app/_lib/data-service";
 import { useUserDataOnServer } from "@/app/hooks/useUserDataOnServer";
 import { type Metadata } from "next";
 import { redirect } from "next/navigation";
-import { Suspense, use } from "react";
+import { use } from "react";
 
 export const metadata: Metadata = {
   title: "Bookmarks",
